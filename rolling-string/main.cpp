f57b912659c38b1b4221e25ce0a18f11f2ef656f
Nigel Belderbos <nigel.belderbos@outlook.com>
2024-09-24 15:47:58 +0200

    chore: Add falling string animation to main.cpp

---
+++ rolling-string/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <windows.h>
+
+void sendChar(char c)
+{
+    std::cout << c;
+    Sleep(50);
+}
+
+void removeLastChar()
+{
+    std::cout << "\b \b";
+}
+
+void fallingString(std::string text)
+{
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < text.length(); i++)
+    {
+        for (int j = 0; j < alphabet.length(); j++)
+        {
+            sendChar(alphabet[j]);
+            if (alphabet[j] == text[i]) break;
+            removeLastChar();
+        }
+    }
+}
+
+
+int main() {
+    fallingString("hottentottenteetentententoonstelling");
+    return 0;
+}
+
+
